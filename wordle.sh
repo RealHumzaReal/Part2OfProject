b209c4ffaae8f8dd0d05fbf715de7ee5b785252b
RealHumzaReal <huali@smail.pembinatrails.ca>
2026-01-21 16:41:24 -0600

    Working randomize system
    after a long gruelling amount of time trying to make this code work, it finally happens after idek how long. I added brackets to the commands i wanted to apply to the variables and with that a dollar sign was what i was missing at the front, so it would replace the entire thing with that specific code that would work, without the brackets or dollarsign, it wasnt giving me the output of the file from the variable but the output from the command since there was a spacing. also everything else was isolated for a time just to test this specific feature.

--- wordle.sh
+++ wordle.sh
@@ -4,14 +4,18 @@ filepath="/home/gamerwolf4512/Documents/Part2OfProject/word_library"
 word="XXXXX"
 
 randomize_word() {
-chosenfile= ls $filepath | shuf -n 1
-echo "$chosenfile"
+
+	chosenfile=$(ls $filepath | shuf -n 1)
+	word=$(shuf -n 1 $filepath/$chosenfile)
+	echo "file: $chosenfile, word: $word"
+
+	#echo "word is $word"
 }
 
+randomize_word
 
-
-while true; do
-	echo -e "\nWelcome to Wordle.\nYou have 5 attempts, guess them all wrong and you lose."
-	sleep 1
-	randomize_word
-done
+#while true; do
+	#echo -e "\nWelcome to Wordle.\nYou have 5 attempts, guess them all wrong and you lose."
+	#sleep 1
+	#randomize_word
+#done
