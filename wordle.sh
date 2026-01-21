db5b9ae5a6056b4a4fecfa63c0600facb77e4462
RealHumzaReal <huali@smail.pembinatrails.ca>
2026-01-21 15:06:45 -0600

    Brainstorming
    Honestly not much work is being done because I'm struggling to think of a system that can randomise the folders within the folder without it looking like too much code or incomprehensible code since I still want it to have good code structure.

--- wordle.sh
+++ wordle.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+filepath="/home/gamerwolf4512/Documents/Part2OfProject/word_library"
+word="XXXXX"
+
+randomize_word() {
+
+}
+
+
+
 while true; do
 	echo -e "\nWelcome to Wordle.\nYou have 5 attempts, guess them all wrong and you lose."
 	sleep 1
