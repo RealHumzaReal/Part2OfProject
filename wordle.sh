284d5b76ccb6cdb3420a4af99bf5cb87f5dbe20f
RealHumzaReal <huali@smail.pembinatrails.ca>
2026-01-21 15:22:06 -0600

    Shuffle command works
    I took the file path, and used a pipe to apply a specific command to it which was shuffle, however i just wanted 1 random out put. So I made it so it would shuffle once using the n option to display only 1 file.

--- wordle.sh
+++ wordle.sh
@@ -4,7 +4,8 @@ filepath="/home/gamerwolf4512/Documents/Part2OfProject/word_library"
 word="XXXXX"
 
 randomize_word() {
-
+chosenfile= ls $filepath | shuf -n 1
+echo "$chosenfile"
 }
 
 
@@ -12,4 +13,5 @@ randomize_word() {
 while true; do
 	echo -e "\nWelcome to Wordle.\nYou have 5 attempts, guess them all wrong and you lose."
 	sleep 1
+	randomize_word
 done
