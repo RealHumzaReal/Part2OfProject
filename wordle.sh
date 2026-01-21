603c7129f7690732020db123299141970ec59a3a
RealHumzaReal <huali@smail.pembinatrails.ca>
2026-01-21 14:34:44 -0600

    Loop for wordle playability
    I will be adding a loop so that the game can restart once it's over, however I'll make sure to be careful because I am aware loops can be negative

--- wordle.sh
+++ wordle.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo -e "\nWelcome to Wordle.\nYou have 5 attempts, guess them all wrong and you lose."
+while true; do
+	echo -e "\nWelcome to Wordle.\nYou have 5 attempts, guess them all wrong and you lose."
+	sleep 1
+done
