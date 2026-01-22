a0db636d294bc8974709effda7dc8f836fea04e5
RealHumzaReal <huali@smail.pembinatrails.ca>
2026-01-22 10:00:47 -0600

    Slowly working on a for loop
    I made a simple for loop however I may need to restructure it since if the user types in something that exceeds/is less then 5 letters

--- wordle.sh
+++ wordle.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 filepath="/home/gamerwolf4512/Documents/Part2OfProject/word_library" #file path to the word library.
-attempts=0
 
 randomize_word() {
 	chosenfile=$(ls $filepath | shuf -n 1) #shuffles all files in folder and takes a random one, making it the variable
 	word=$(shuf -n 1 $filepath/$chosenfile) #shuffles all the words in the file and takes a random one for the wordle
 } #shuffles word
 
+guess_word_tries(){
+	for i in {1..5}; do
+		read -p "Type word here: " answer
+		check_if_word_is_correct
+	done
+}
 
-
+check_if_word_is_correct(){
+	if [ $answer ]
+}
 
 while true; do
-	echo -e "\nWelcome to Wordle.\nYou have 5 attempts, guess them all wrong and you lose."
+	echo -e "\nWelcome to Wordle.\nYou have 5 attempts, guess them all wrong and you lose.\n"
 	sleep 1
 	randomize_word
-	read -p "1st guess:" $guess1
+	guess_word_tries
 done
