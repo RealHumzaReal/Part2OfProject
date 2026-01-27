46684d632bc03caa054bae7e1f294ab66c33f7b8
RealHumzaReal <huali@smail.pembinatrails.ca>
2026-01-27 13:32:34 -0600

    I modified some part of the script
    I modified the if statement, and lowkey realized I was being dumb. I should have been using at ** for the yellow instead of green, the green should match the letters completely. Also to add I have no idea why I called the variable within itself in wordle output, genuinely dont know what morning me was on. Anyways I fixed said issue now

--- wordle.sh
+++ wordle.sh
@@ -10,35 +10,35 @@ green='\e[0;32m'
 
 randomize_word() {
 	chosenfile=$(ls $filepath | shuf -n 1) #shuffles all files in folder and takes a random one, making it the variable
-	word=$(shuf -n 1 $filepath/$chosenfile) #shuffles all the words in the file and takes a random one for the wordle
+	answer=$(shuf -n 1 $filepath/$chosenfile) #shuffles all the words in the file and takes a random one for the wordle
 } #shuffles word
 
 guess_word_tries(){
 	for (( i=1; i<6; )); do #loops for every unless i is greater then 6. i is supposed to represent the gueses
-		read -p "Type word here ($i): " answer #reads users answer and i indicates what attempt there on
+		read -p "Type word here ($i): " guess #reads users guess and i indicates what attempt there on
 		check_if_word_is_in_limit
 	done
 } #amount of attempts
 
 check_if_word_is_in_limit(){
-	if [ ${#answer} == 5 ]; then #if the answer's length is 5, it will allow the user to continue
+	if [ ${#guess} == 5 ]; then #if the guess's length is 5, it will allow the user to continue
 		check_if_word_is_in_word_library
-	else #if the answer is not any of the above, it will play this message
+	else #if the guess is not any of the above, it will play this message
 		echo -e "What you typed in was over/under the limit\nPlease type in again!\n"
 	fi
-} #checks answer is in limit
+} #checks guess is in limit
 
 check_if_word_is_in_word_library(){
-	if [ -e $filepath/${answer::1}.sh ]; then #if the first letter in the word is valid, meaning it exists on the dictionary. It will allow the user to continue
+	if [ -e $filepath/${guess::1}.sh ]; then #if the first letter in the word is valid, meaning it exists on the dictionary. It will allow the user to continue
 		echo "it works"
 		check_if_word_is_in_dictionary
 	else #if the first letter is a value or symbol, it wont allow you to do anything.
 		echo -e "What you typed in was a value/symbol.\nPlease type again!\n"
 	fi
-} #check if answer starts with a letter
+} #check if guess starts with a letter
 
 check_if_word_is_in_dictionary(){
-	if grep -q "$answer" $filepath/${answer::1}.sh; then #uses grep to search for specific phrase in said word library, if it doesn't exist, it'll say it's not a word.
+	if grep -q "$guess" $filepath/${guess::1}.sh; then #uses grep to search for specific phrase in said word library, if it doesn't exist, it'll say it's not a word.
 		word_from_answer
 	else
 		echo "That word is not in the dictionary!"
@@ -46,19 +46,21 @@ check_if_word_is_in_dictionary(){
 } #checks of answer is in the dictionary
 
 word_from_answer(){
-	if [ $word == $answer ]; then
-		echo -e "${green}$word${no_color}\nCongradulations you guessed the word!\nWould you like to guess again?"
-	elif [ $word != $answer ]; then
+	if [ $answer == $guess ]; then
+		echo -e "${green}$answer${no_color}\nCongradulations you guessed the word!\nWould you like to guess again?"
+	elif [ $answer != $guess ]; then
 		individually_check_letters
 	fi
 }
 
 individually_check_letters(){
 	for (( c=0; c<5; c++ )); do
-		if [[ $answer == *"${word:c:1}"* ]]; then
-			wordle_output=$green$wordle_output${word:c:1}
-		elif [[ $answer != *"${word:c:1}"* ]]; then
-			wordle_output=$no_color$wordle_output${answer:c:1}
+		if [[ $guess == "${answer:c:1}" ]]; then
+			wordle_output+="$green${answer:c:1}$no_color"
+		elif [[ $answer == *"${guess:c:1}"* ]]; then
+			wordle_output+="$yellow${answer:c:1}$no_color"
+		else
+			wordle_output+="$no_color${guess:c:1}"
 		fi
 		echo -e "$wordle_output"
 	done
