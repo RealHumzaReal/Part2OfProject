a20e376a258a49756ecd0343524c9dc18e477acb
RealHumzaReal <huali@smail.pembinatrails.ca>
2026-02-01 23:43:26 -0600

    A forgotten update lol
    I kinda also forgot to submit this and put the comments, sorry. If anything is missing from my final project, like a question in my part 1 submission or something I was suppose to do on my code, let me know through Edsby. Otherwise I'll see you maybe next semester again.

--- wordle.sh
+++ wordle.sh
@@ -9,6 +9,7 @@ no_color="\e[0m"
 yellow="\e[1;33m"
 green="\e[0;32m"
 
+#prompts the user of the beggining of wordle.
 start_of_game() {
 	echo -e "\nWelcome to$green Wordle.$no_color\nYou have$green 5$no_color attempts, guess them all wrong and you$yellow lose.\n $no_color"
 	sleep 2
@@ -16,88 +17,93 @@ start_of_game() {
 	guess_word_tries
 }
 
-
+#function shuffles files first then shuffles the words within the file using grep.
 randomize_word() {
-	chosenfile=$(ls $filepath | shuf -n 1) #shuffles all files in folder and takes a random one, making it the variable
-	answer=$(shuf -n 1 $filepath/$chosenfile) #shuffles all the words in the file and takes a random one for the wordle
-} #shuffles word
+	chosenfile=$(ls $filepath | shuf -n 1) 
+	answer=$(shuf -n 1 $filepath/$chosenfile) 
+}
 
+#uses a for loop with the index representing wordle guesses, asks for user input before going to the check if word is in limit function. If the index is equal to 6 it then tells the player they lost and asks if they would like to continue.
 guess_word_tries(){
-	for (( i=1; i<6; )); do #loops for every unless i is greater then 6. i is supposed to represent the gueses
-		read -p "Type word here ($i): " guess #reads users guess and i indicates what attempt there on
+	for (( i=1; i<6; )); do 
+		read -p "Type word here ($i): " guess 
 		check_if_word_is_in_limit
-		if [ $i == 6 ]; then #this checks if i is equal to 6 guesses, if so then it tells the user their guesses are done and gives them the option to reset
+		if [ $i == 6 ]; then 
 			echo -e "You have failed to find the word!\nThe word was $answer\nWould you like to play again? Yes(Y) No(N)"
 			exit_or_continue_game
 		fi
 	done
-} #amount of attempts
+}
 
+#checks if the words length is 5 letters long, if it isn't it asks for user to try again
 check_if_word_is_in_limit(){
-	if [ ${#guess} == 5 ]; then #if the guess's length is 5, it will allow the user to continue
+	if [ ${#guess} == 5 ]; then 
 		((i--))
 		check_if_word_is_in_word_library
-	else #if the guess is not any of the above, it will play this message
+	else 
 		echo -e "What you typed in was over/under the limit\nPlease type in again!\n"
 	fi
-} #checks guess is in limit
+}
 
+#checks if the first letter within word matches with a file in the directory, if not it asks the user to try again, however this also consists of capitals too.
 check_if_word_is_in_word_library(){
-	if [ -e $filepath/${guess::1}.sh ]; then #if the first letter in the word is valid, meaning it exists on the dictionary. It will allow the user to continue
+	if [ -e $filepath/${guess::1}.sh ]; then
 		check_if_word_is_in_dictionary
-	else #if the first letter is a value or symbol, it wont allow you to do anything, however this logic is infact flawed because if you put a captial at the startm it will count it as a value. I wasn't able to fix this in time however I recognize this is an error
+	else 
 		echo -e "What you typed in was a value/symbol.\nPlease type again!\n"
 	fi
-} #check if guess starts with a letter
+}
 
+#uses grep in an if statement for one last time to check if a word is in said letters dictionary, if not it wouldn't count as a word.
 check_if_word_is_in_dictionary(){
-	if grep -q "$guess" $filepath/${guess::1}.sh; then #uses grep to search for specific phrase in said word library, if it doesn't exist, it'll say it's not a word.
+	if grep -q "$guess" $filepath/${guess::1}.sh; then 
 		word_from_answer
 	else
 		echo "That word is not in the dictionary!"
 	fi
-} #checks of answer is in the dictionary
+}
 
+#uses an if statement to see if the guess is the same or not as the answer, if so the player wins, if not then it checks through the next function what letters match or not
 word_from_answer(){
-	if [ $answer == $guess ]; then #if statement that checks if guess string is = to answer
-		echo -e "${green}$answer${no_color}\nCongradulations you guessed the word!\nWould you like to guess again?\n" #if statement true then it says this line of code
-		read -p "Just type Yes(Y) or No(N)" continue_playing #asks player if they want to continue playing
+	if [ $answer == $guess ]; then
+		echo -e "${green}$answer${no_color}\nCongradulations you guessed the word!\nWould you like to guess again?\n" 
+		read -p "Just type Yes(Y) or No(N)" continue_playing 
 		exit_or_continue_game
-	elif [ $answer != $guess ]; then #this if statement checks if your guess isnt equal to the answer (could just say else, but I may need to use it later, so i will leave it like this for now. Unless otherwise)
+	elif [ $answer != $guess ]; then
 		individually_check_letters
 	fi
 }
 
+#does a for loop for 5 times, indicating each letter in the 5 lettered word, it works perfectly for the green letters however with the yellow letters it wont since it can consist the l in 'allys' as 2 yellows even if the word is later. Being a huge problem that I have yet to figure out how to fix. It then makes the text blank if none are true, before giving the output to the user, and finally adding i as a guess used.
 individually_check_letters(){
-	for (( c=0; c<5; c++ )); do #loops for 5 times (for each letter)
-
+	for (( c=0; c<5; c++ )); do 
 		guess_letter=${guess:c:1}
 		answer_letter=${answer:c:1}
-
-		if [[ $guess_letter == "$answer_letter" ]]; then #this if statement checks if the guess letter is = answer letter
-			wordle_output+="$green$guess_letter$no_color" #if the statement is true, it returns with the wordle output being added with a green letter
-		elif [[ $answer == *"$guess_letter"* ]]; then #this if statement checks if the letter is at least somewhere in the answer
-			wordle_output+="$yellow$guess_letter$no_color" #if the statement is true, it returns with the wordle output being added with a yellow letter however this is also flawed since it looks at duplicated letters too, such as not counting if it looks a letter alreaddy
-		else #unless the statements dont follow any of the following
-			wordle_output+="$guess_letter" #unless it really doesnt come true for any of the if statements it comes back with a blank letter
+		if [[ $guess_letter == "$answer_letter" ]]; then 
+			wordle_output+="$green$guess_letter$no_color" 
+		elif [[ $answer == *"$guess_letter"* ]]; then 
+			wordle_output+="$yellow$guess_letter$no_color"
+		else 
+			wordle_output+="$guess_letter"
 		fi
 	done
-	echo -e "$wordle_output\n" #says the output
-	wordle_output="" #resets output so it doesn't remain the same for the next loop
-	((i++)) #adds i so that a try has been used.
+	echo -e "$wordle_output\n" 
+	wordle_output=""
+	((i++)) 
 }
 
+#case statement that takes user input to see if player wants to continue playing or not, if so it makes i=6 so for loop from earlier resets and starts at the beggining again. If not it just exits out the code entirely and if none of the above it forces the player to re say answer however I think I just accidently made an infite case statement looking back
 exit_or_continue_game(){
-	case $continue_playing in #case statement that determines what the user chooses to do
-		y|Y|yes|Yes) echo -e "\nThe game will start again then!" #if user says any of the following options, it will restart the game
-		((i=6)) #makes sure to make i=6 so that for loop ends.
+	case $continue_playing in 
+		y|Y|yes|Yes) echo -e "\nThe game will start again then!"
+		((i=6)) 
 		sleep 2
 		start_of_game
 		;;
-		n|N|no|No) echo -e "\nYou have chosen to exit the game, goodbye!" #if the user says any of the following options, it will end the game
-		exit #exits code
+		n|N|no|No) echo -e "\nYou have chosen to exit the game, goodbye!"
+		exit
 		;;
-		*) read -p "Please re-enter your answer:" continue_playing #if user says anything else it will force the user to type in the answer and repeat.
+		*) read -p "Please re-enter your answer:" continue_playing 
 		exit_or_continue_game
 		;;
 	esac
